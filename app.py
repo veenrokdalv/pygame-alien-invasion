2acb4ff6801fbe06c978c0cc71268d1ba7b14fc1
Alex Shavrin <alex.shavrin200303mail.ru>
2020-11-21 23:10:23 +0300

    В классе App: Добавлены методы: quit(), check_events(), run().

--- app.py
+++ app.py
@@ -8,16 +8,43 @@ class App:
     """
     def __init__(self):
         """
-        Инициализация главных настроек приложения
+        Инициализация главных настроек приложения.
         """
         
-        # Настройка окна игры
+        # Настройка окна игры.
         self.WIDTH = 1200
         self.HIGHT = 600
         self.TITLE = 'alien invasion'
         self.WINDOW = pg.display.set_mode((self.WIDTH, self.HIGHT))
         pg.display.set_caption(self.TITLE)
 
-        
+
+    def quit(self):
+        """
+        Завершает программу.
+        """
+
+        pg.quit()
+        quit()
+
+
+    def check_events(self):
+        """
+        Обработка событий программы.
+        """
+
+        # Нажатие на крестик.
+        [self.quit() for event in pg.event.get() if event.type == pg.QUIT]
+
+
+    def run(self):
+        """
+        Запуск главного цикла приложения.
+        """
+
+        while True:
+            self.check_events()
+
+
 
 
